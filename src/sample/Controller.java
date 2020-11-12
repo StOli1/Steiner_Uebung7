8c0ada92a2c7e09df80dfbdc15965663c6606fda
Oliver Steiner <oliver.steiner@htl-wels.at>
2020-11-12 15:54:34 +0100

    finished

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller extends CurrencyConverter {
+
+    @FXML Button convert = new Button();
+
+    @FXML TextField textFieldEuro = new TextField();
+    @FXML TextField textFieldYen = new TextField();
+
+    public void doConvertion()
+    {
+        convert.setOnAction(e -> {
+            double euro = Double.parseDouble(textFieldEuro.getText());
+            textFieldYen.setText(String.valueOf(euroToYen(euro)));
+        });
+    }
+
 }
---
+++ src/sample/CurrencyConverter.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class CurrencyConverter {
+
+    public double euroToYen(double euro)
+    {
+      double yen = euro * 124;
+
+      return yen;
+    }
+}
